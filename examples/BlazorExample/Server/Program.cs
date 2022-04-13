4e948d581bf3e7266acf338141bbc9411626ecc4
Joseph Shook <Joseph.Shook@Surescripts.com>
2022-04-13 12:51:56 -0700

    Adding docker files to setup lab environment.

--- examples/BlazorExample/Server/Program.cs
+++ examples/BlazorExample/Server/Program.cs
@@ -116,7 +116,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 }
 
-// app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
